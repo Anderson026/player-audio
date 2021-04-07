f325fff964d3f5c87ed05c3f11188e7849175846
Anderson026 <anderson-ferreira22@hotmail.com>
2021-04-07 20:10:24 -0300

    removendo a barra de progresso que estava com um bug que consumia toda a memoria do computador

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 
 let audio = document.querySelector("#audio");
-let time_bar = document.querySelector("#time_bar");
+
 // funções do controle de player
 function play() {
   audio.play();
@@ -30,29 +30,4 @@ function volume_down() {
 function volme_up() {
   audio.volume += 0.2;
 }
-// fim das funções de player
-// barra de progresso
-function progress_bar(event) {
-  let percentage = Math.floor((event.offsetX / time_bar.offsetWidth) * 100);
-  audio.currentTime = audio.duration * percentage / 100;
-  time_bar.value = audio.currentTime;
-}
-// mostrar os minutos e segundos da música
-function timeUpdate() {
-  const progressBar = document.getElementById("time_bar");
-  const timeLabel = document.getElementsByClassName("time")[0];
-
-  const currentTime = audio.currentTime;
-  progressBar.value = currentTime ; audio.duration * 100;
-
-  const minutes = Math.floor(currentTime / 60);
-  const formateMinutes = (minutes < 10 ? "0" : "") + minutes;
-  const seconds = Math.floor(currentTime % 60);
-  const formatedSeconds = (seconds < 10 ? "0" : "") + seconds;
-  timeLabel.innerHTML = formateMinutes + ":" + formatedSeconds;
-
-  setInterval(timeUpdate, 1000);
-}
-
-
-
+// fim das funções do player
